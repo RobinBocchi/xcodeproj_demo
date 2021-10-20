0170f0149092de2b1c05e7aff5d8081bd15a17a8
jiaxiaobin <jiaxiaobin@jd.com>
2021-10-20 10:09:25 +0800

    新增RKTestViewController，查看pbxproj的变化

---
+++ RKTestArch/RKTestViewController.h
@@ -0,0 +1,16 @@
+//
+//  RKTestViewController.h
+//  RKTestArch
+//
+//  Created by jiaxiaobin on 2021/10/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RKTestViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RKTestArch/RKTestViewController.m
@@ -0,0 +1,31 @@
+//
+//  RKTestViewController.m
+//  RKTestArch
+//
+//  Created by jiaxiaobin on 2021/10/20.
+//
+
+#import "RKTestViewController.h"
+
+@interface RKTestViewController ()
+
+@end
+
+@implementation RKTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
