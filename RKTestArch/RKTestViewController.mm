c439d51c71278dcf1dde1d8a5e1fe256d87b1e79
jiaxiaobin <jiaxiaobin@jd.com>
2021-10-20 12:23:00 +0800

    增加libz和accelerate两个framework，百度文档上指定的依赖库列表缺少了这两个

--- RKTestArch/RKTestViewController.mm
+++ RKTestArch/RKTestViewController.mm
@@ -7,10 +7,10 @@
 
 #import "RKTestViewController.h"
 #import <BaiduMapAPI_Base/BMKBaseComponent.h>//引入base相关所有的头文件
-#import <BaiduMapAPI_Map/BMKMapComponent.h>//引入地图功能所有的头文件
-#import <BaiduMapAPI_Search/BMKSearchComponent.h>//引入检索功能所有的头文件
-#import <BaiduMapAPI_Utils/BMKUtilsComponent.h>//引入计算工具所有的头文件
-#import <BaiduMapAPI_Map/BMKMapView.h>//只引入所需的单个头文件
+//#import <BaiduMapAPI_Map/BMKMapComponent.h>//引入地图功能所有的头文件
+//#import <BaiduMapAPI_Search/BMKSearchComponent.h>//引入检索功能所有的头文件
+//#import <BaiduMapAPI_Utils/BMKUtilsComponent.h>//引入计算工具所有的头文件
+//#import <BaiduMapAPI_Map/BMKMapView.h>//只引入所需的单个头文件
 
 @interface RKTestViewController ()
 
